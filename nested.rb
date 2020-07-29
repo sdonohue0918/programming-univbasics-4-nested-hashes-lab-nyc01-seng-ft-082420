29c3a08e2db2b3a105b2d9c9152d3cce60cc1ecd
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-29 19:00:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def adding_matz
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
 
-puts programmer_hash
+return programmer_hash
 end
 
 def changing_alan
